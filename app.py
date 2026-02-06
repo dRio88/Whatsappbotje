c1baa5f95df2e2c1aea96d4f71cefa5a42535934
dRio88 <dariovenneman@hotmail.com>
2026-02-06 18:08:08 +0100

    UpdateV3 app.py

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ def whatsapp():
 
 # --------- APP START ---------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
